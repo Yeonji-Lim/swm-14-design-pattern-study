ecdf86ec0a5f5f87134dfee14e8828d574f5b104
hexaspace <hexa6do@naver.com>
2023-07-11 18:22:57 +0900

    Feat: doyeon 어뎁터 패턴 - chatroom 예시

---
+++ week7-adapter-pattern/doyeon/ACrew.ts
@@ -0,0 +1,22 @@
+import CrewChatRoom from "./CrewChatRoom";
+
+export default class ACrew extends CrewChatRoom {
+  constructor() {
+    super();
+  }
+  public create(): void {
+    console.log("create : A크루 채팅방 생성");
+  }
+  public join(user: string): void {
+    console.log("join : A크루 채팅방에 " + user + "님이 입장하셨습니다.");
+  }
+  public sendMessage(user: string, msg: string): void {
+    console.log(
+      "sendMessage : A크루 채팅방에 " +
+        user +
+        '님이 "' +
+        msg +
+        '"라고 말했습니다.'
+    );
+  }
+}
